8ac04cb2a7296f28d0c4ef5913933d821e0c38ed
OmverYo <justinlee990106@gmail.com>
2022-09-26 22:04:30 +1300

    Updating JavaScript Snake

--- assets/JavaScript-Snake/assets/js/snake.js
+++ assets/JavaScript-Snake/assets/js/snake.js
@@ -1,22 +1,54 @@
 var canvas = document.getElementById("snake");
 var context = canvas.getContext("2d");
 
+// Draws the game secttion
 context.fillStyle = "#483D8B";
 context.fillRect(0, 0, canvas.clientWidth, canvas.height);
 
-var snake = {
-    xPosition: 400,
-    yPosition: 400,
+// Declares the pixels of snake bodies
+var snakePixel = 20;
 
+// Snake object and contents
+var snake = {
     // Snake speed
     xVelocity: 0,
     yVelocity: 0,
 
-    // Limits maximum snake size
-    maxSnakeSize: 4
+    // Snake size array keeps the number of snake body (length)
+    snakeSize: [
+        { xPosition: 400, yPosition: 400 },
+        { xPosition: 390, yPosition: 400 },
+        { xPosition: 380, yPosition: 400 },
+        { xPosition: 370, yPosition: 400 },
+        { xPosition: 360, yPosition: 400 }
+    ]
+
 };
 
+// Apple object and contents
 var apple = {
-    xPosition: 200,
-    yPosition: 200
+    // Apple's initial position
+    xPosition: 400,
+    yPosition: 600
 };
+
+// Function to draw the snake
+function drawSnake() {
+    snake.snakeSize.forEach(drawSnakeTails);
+}
+
+// Function to draw the snake's tails or bodies
+function drawSnakeTails(snakeTails) {
+    context.fillStyle = "green";
+    context.fillRect(snakeTails.xPosition, snakeTails.yPosition, snakePixel - 1, snakePixel - 1);
+}
+
+// Fnction to draw the apple
+function drawApple() {
+    // Draws the apple
+    context.fillStyle = "red";
+    context.fillRect(apple.xPosition, apple.yPosition, snakePixel - 1, snakePixel - 1);
+}
+
+drawApple();
+drawSnake();
